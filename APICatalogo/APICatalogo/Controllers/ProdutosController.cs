13c324065544c04ac83e56ebfffd3537aa670afc
Geraldo Luiz <113297903+Geeh17@users.noreply.github.com>
2023-06-07 23:26:15 -0300

    ProdutosController - Get,Post,Put e Delete

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -1,7 +1,7 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -10,21 +10,74 @@ namespace APICatalogo.Controllers
     public class ProdutosController : ControllerBase
     {
         private readonly AppDbContext _context;
-
         public ProdutosController(AppDbContext context)
         {
             _context = context;
         }
 
-        [HttpGet]   
-        public ActionResult <IEnumerable<Produto>> Get()
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
         {
             var produtos = _context.Produtos.ToList();
-            if(produtos is null)
+            if (produtos is null)
             {
-                return NotFound("Produtos não encontrados...");
+                return NotFound();
             }
             return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoID == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+                return BadRequest();
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoID }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoID == id);
+            //var produto = _context.Produtos.Find(id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado...");
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,32 +1,33 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace APICatalogo.Models;
-
-[Table("Produtos")]
-public class Produto
+namespace APICatalogo.Models
 {
-    [Key]
-    public int ProdutoID { get; set; }
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoID { get; set; }
 
-    [Required]
-    [StringLength(80)]
-    public string? Nome { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string? Nome { get; set; }
 
-    [Required]
-    [StringLength(300)]
-    public string? Descricao { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? Descricao { get; set; }
 
-    [Required]
-    [Column(TypeName ="decimal(10,2")]
-    public decimal Preco { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
 
-    [Required]
-    [StringLength(300)]
-    public string? ImagemUrl { get; set; }
-    public float? Estoque { get; set; }
-    public DateTime DataCadastro { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string? ImagemUrl { get; set; }
 
-    public Categoria? Categoria { get; set; }   
-
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
 }
